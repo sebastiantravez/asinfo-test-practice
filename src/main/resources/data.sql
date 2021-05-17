cce1522b7c524cf94d1cdd9467205dc5b8f42772
Sebastian <sebastiantravez0@gmail.com>
2021-05-17 15:36:19 -0500

    update yml

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,2 @@
 CREATE DATABASE test-practice
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-INSERT INTO sec_roles (id, name, status) VALUES
-  (uuid_generate_v4(),'ROLE_ADMIN', 'ACT'),
-  (uuid_generate_v4(),'ROLE_USER', 'ACT'),
-  (uuid_generate_v4(),'ROLE_CUSTOMER', 'ACT'),
-  (uuid_generate_v4(),'ROLE_SUPER', 'ACT');
 
