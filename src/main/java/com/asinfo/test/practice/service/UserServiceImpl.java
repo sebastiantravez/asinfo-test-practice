b6d271f78cc3ee22b767b0731c49d2e235069479
Sebastian <sebastiantravez0@gmail.com>
2021-05-16 23:19:34 -0500

    save users roles presenter

--- src/main/java/com/asinfo/test/practice/service/UserServiceImpl.java
+++ src/main/java/com/asinfo/test/practice/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.asinfo.test.practice.service;
 
 import com.asinfo.test.practice.controller.entity.Users;
+import com.asinfo.test.practice.controller.entity.UsersRoles;
 import com.asinfo.test.practice.controller.repository.UsersRepository;
+import com.asinfo.test.practice.controller.repository.UsersRolesRepository;
 import com.asinfo.test.practice.view.UsersPresenter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UsersRepository usersRepository;
+    @Autowired
+    UsersRolesRepository usersRolesRepository;
 
     @Override
     public void updateUser(UsersPresenter usersPresenter) {
@@ -20,5 +24,11 @@ public class UserServiceImpl implements UserService {
                 .password(usersPresenter.getPassword())
                 .build();
         usersRepository.save(users);
+        usersPresenter.getRolesPresenter().forEach(item -> {
+            usersRolesRepository.save(UsersRoles.builder()
+                    .idRol(item.getIdRol())
+                    .idUser(users.getIdUser())
+                    .build());
+        });
     }
 }
